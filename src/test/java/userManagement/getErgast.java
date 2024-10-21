0813dadc29007add1a8348913acd0c78930acb82
vishnutallam <vishnupriyatallam@gmail.com>
2024-10-21 12:54:39 +0530

    master branch first code changes

--- src/test/java/userManagement/getErgast.java
+++ src/test/java/userManagement/getErgast.java
@@ -38,4 +38,16 @@ public class getErgast extends BaseTest {
         assertEquals(actualStatusCode, 200); //Testng
         System.out.println(resp.body().asString());
     }
+    @Test(description=" master branch first code changes")
+    public void validateResponseBodyGetPathParam1() {
+
+
+        Response resp = given()
+                .pathParam("raceSeason", 2016)
+                .when()
+                .get("http://ergast.com/api/f1/{raceSeason}/circuits.json"); //RestAssured
+        int actualStatusCode = resp.statusCode();  //RestAssured
+        assertEquals(actualStatusCode, 200); //Testng
+        System.out.println(resp.body().asString());
+    }
 }
